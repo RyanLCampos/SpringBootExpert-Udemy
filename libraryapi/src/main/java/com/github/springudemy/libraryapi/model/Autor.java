60b2660bd800cf26ad19b682066762b680c89478
RyanLCampos <ryanlcampos19@gmail.com>
2025-02-24 21:56:06 -0300

    Feat: Adicionar campos de auditoria e id_usuario no model Autor

--- libraryapi/src/main/java/com/github/springudemy/libraryapi/model/Autor.java
+++ libraryapi/src/main/java/com/github/springudemy/libraryapi/model/Autor.java
@@ -1,11 +1,17 @@
 package com.github.springudemy.libraryapi.model;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,9 +22,10 @@ import lombok.Data;
 import lombok.ToString;
 
 @Entity
-@Table(name = "autor" , schema = "public") // Não necessario colocar public (Default)
+@Table(name = "autor", schema = "public") // Não necessario colocar public (Default)
 @Data
 @ToString(exclude = "livros")
+@EntityListeners(AuditingEntityListener.class) // Ficara vendo se ocorrer alguma alteração
 public class Autor {
 
     @Id
@@ -39,4 +46,15 @@ public class Autor {
     // FetchType.LAZY - Irá puxar os livros somente quando requisitado.
     @OneToMany(mappedBy = "autor", fetch = FetchType.LAZY) // Um autor para muitos livros
     private List<Livro> livros;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuario;
 }

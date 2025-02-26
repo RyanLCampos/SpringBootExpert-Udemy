a5dda65a25ad57812c2fdc53c3f993c557b80e2a
RyanLCampos <ryanlcampos19@gmail.com>
2025-02-25 21:09:31 -0300

    :sparkles: Feat: Implementa AutorValidator para validar regras de negócios de Autor

---
+++ libraryapi/src/main/java/com/github/springudemy/libraryapi/validator/AutorValidator.java
@@ -0,0 +1,40 @@
+package com.github.springudemy.libraryapi.validator;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.github.springudemy.libraryapi.exceptions.RegistroDuplicadoException;
+import com.github.springudemy.libraryapi.model.Autor;
+import com.github.springudemy.libraryapi.repository.AutorRepository;
+
+@Component
+public class AutorValidator {
+
+    @Autowired
+    private AutorRepository repository;
+
+    public void validar(Autor autor) {
+        if (existeAutorCadastrado(autor)) {
+            throw new RegistroDuplicadoException("Autor já cadastrado!");
+        }
+    }
+
+    private boolean existeAutorCadastrado(Autor autor) {
+        Optional<Autor> autorEncontrado = repository.findByNomeAndDataNascimentoAndNacionalidade(
+                autor.getNome(),
+                autor.getDataNascimento(),
+                autor.getNacionalidade()
+        );
+
+        // Verifica se o autor (Parametro) não possui id (Criação)
+        if(autor.getId() == null){ 
+            return autorEncontrado.isPresent(); // Se for encontrado, já está cadastrado.
+        }
+
+        // Verifica se o autor está atualizando e se o id não coincide
+        return autorEncontrado.isPresent() && !autor.getId().equals(autorEncontrado.get().getId());
+    }
+
+}

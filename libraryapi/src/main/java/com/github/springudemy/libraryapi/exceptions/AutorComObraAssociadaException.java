cf69fe721639ae45e51cc3c8908adbc4c1c5a640
RyanLCampos <ryanlcampos19@gmail.com>
2025-02-25 21:06:59 -0300

    :sparkles: Feat: Cria classes de exceções personalizadas para tratamento de erros

---
+++ libraryapi/src/main/java/com/github/springudemy/libraryapi/exceptions/AutorComObraAssociadaException.java
@@ -0,0 +1,8 @@
+package com.github.springudemy.libraryapi.exceptions;
+
+public class AutorComObraAssociadaException extends RuntimeException{
+
+    public AutorComObraAssociadaException(String message){
+        super(message);
+    }
+}
---
+++ libraryapi/src/main/java/com/github/springudemy/libraryapi/exceptions/RegistroDuplicadoException.java
@@ -0,0 +1,9 @@
+package com.github.springudemy.libraryapi.exceptions;
+
+public class RegistroDuplicadoException extends RuntimeException{
+
+    public RegistroDuplicadoException(String message) {
+        super(message);
+    }
+
+}

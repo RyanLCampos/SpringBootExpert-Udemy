7b4c09df9487caee546003bef4c6e96eb9d3e364
RyanLCampos <ryanlcampos19@gmail.com>
2025-02-28 00:21:50 -0300

    :sparkles: Feat: implementa endpoint para obter detalhes de um livro e conclui endpoint de cadastrar livro

--- libraryapi/src/main/java/com/github/springudemy/libraryapi/controller/LivroController.java
+++ libraryapi/src/main/java/com/github/springudemy/libraryapi/controller/LivroController.java
@@ -1,14 +1,18 @@
 package com.github.springudemy.libraryapi.controller;
 
 import java.net.URI;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.springudemy.libraryapi.controller.dto.CadastroLivroDTO;
+import com.github.springudemy.libraryapi.controller.dto.ResultadoPesquisaLivroDTO;
 import com.github.springudemy.libraryapi.controller.mappers.LivroMapper;
 import com.github.springudemy.libraryapi.model.Livro;
 import com.github.springudemy.libraryapi.service.LivroService;
@@ -36,5 +40,14 @@ public class LivroController implements GenericController {
 
         return ResponseEntity.created(location).build();
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResultadoPesquisaLivroDTO> obterDetalhes(@PathVariable("id") String id){
+        return service.obterPorId(UUID.fromString(id))
+                .map(livro -> {
+                    ResultadoPesquisaLivroDTO dto = mapper.toDTO(livro);
+                    return ResponseEntity.ok(dto);
+                }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- libraryapi/src/main/java/com/github/springudemy/libraryapi/service/LivroService.java
+++ libraryapi/src/main/java/com/github/springudemy/libraryapi/service/LivroService.java
@@ -1,5 +1,8 @@
 package com.github.springudemy.libraryapi.service;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.github.springudemy.libraryapi.model.Livro;
@@ -14,7 +17,10 @@ public class LivroService {
     private final LivroRepository livroRepository;
 
     public Livro salvar(Livro livro){
-
         return livroRepository.save(livro);
+    }
+
+    public Optional<Livro> obterPorId(UUID id){
+        return livroRepository.findById(id);
     }
 }

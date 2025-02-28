0b86e594c861b5b0c47af80c2d640801f4687b19
RyanLCampos <ryanlcampos19@gmail.com>
2025-02-28 00:20:26 -0300

    :sparkles: Feat: implementa funções de mapeamento no LivroMapper

--- libraryapi/src/main/java/com/github/springudemy/libraryapi/controller/mappers/LivroMapper.java
+++ libraryapi/src/main/java/com/github/springudemy/libraryapi/controller/mappers/LivroMapper.java
@@ -6,21 +6,18 @@ import org.mapstruct.ReportingPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.github.springudemy.libraryapi.controller.dto.CadastroLivroDTO;
+import com.github.springudemy.libraryapi.controller.dto.ResultadoPesquisaLivroDTO;
 import com.github.springudemy.libraryapi.model.Livro;
 import com.github.springudemy.libraryapi.repository.AutorRepository;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = AutorMapper.class)
 public abstract class LivroMapper {
 
     @Autowired
     AutorRepository autorRepository;
-    
+
     @Mapping(target = "autor", expression = "java( autorRepository.findById(dto.idAutor()).orElse(null) )")
     public abstract Livro toEntity(CadastroLivroDTO dto);
-    
+
+    public abstract ResultadoPesquisaLivroDTO toDTO(Livro livro);
 }
-
-
-    
-
-

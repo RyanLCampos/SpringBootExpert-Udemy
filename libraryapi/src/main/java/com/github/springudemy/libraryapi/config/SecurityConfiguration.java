249f22c408498f4456c2843b41db30e9a3e251d4
RyanLCampos <ryanlcampos19@gmail.com>
2025-02-28 22:16:58 -0300

    :sparkles: Feat: cria configuração basica do Security com SecurityFilterChain

---
+++ libraryapi/src/main/java/com/github/springudemy/libraryapi/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.github.springudemy.libraryapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults()) // Configuração de login padrão
+                .httpBasic(Customizer.withDefaults()) // Configuração de autenticação básica padrão
+                .authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated(); // Qualquer requisição precisa estar autenticada
+                })
+                .build();
+    }
+
+}

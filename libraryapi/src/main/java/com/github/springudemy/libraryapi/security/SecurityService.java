69294ddf5b995b2317302f95d0c8e4277749e6b4
RyanLCampos <ryanlcampos19@gmail.com>
2025-03-06 14:13:28 -0300

    :wrench: Chore: remove import não utilizado

--- libraryapi/src/main/java/com/github/springudemy/libraryapi/security/SecurityService.java
+++ libraryapi/src/main/java/com/github/springudemy/libraryapi/security/SecurityService.java
@@ -5,7 +5,6 @@ import com.github.springudemy.libraryapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component

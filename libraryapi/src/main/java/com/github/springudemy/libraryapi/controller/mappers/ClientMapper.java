0f7edb858c7be47682d14f3647a16e1a310bc772
RyanLCampos <ryanlcampos19@gmail.com>
2025-03-10 12:15:04 -0300

    :bug: Fix: corrige conflito de nome de variaveis

--- libraryapi/src/main/java/com/github/springudemy/libraryapi/controller/mappers/ClientMapper.java
+++ libraryapi/src/main/java/com/github/springudemy/libraryapi/controller/mappers/ClientMapper.java
@@ -3,11 +3,13 @@ package com.github.springudemy.libraryapi.controller.mappers;
 import com.github.springudemy.libraryapi.controller.dto.ClientDTO;
 import com.github.springudemy.libraryapi.model.Client;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ClientMapper {
 
+    @Mapping(source = "clientId", target = "clientId")
     Client toEntity(ClientDTO dto);
 
 }

ae0f992e743f9c61188ca60138674f3ec62f41a6
RyanLCampos <ryanlcampos19@gmail.com>
2025-03-10 16:56:41 -0300

    :sparkles: Feat: implementa a configuração do AuthorizationServer

--- libraryapi/src/main/java/com/github/springudemy/libraryapi/config/AuthorizationServerConfiguration.java
+++ libraryapi/src/main/java/com/github/springudemy/libraryapi/config/AuthorizationServerConfiguration.java
@@ -1,11 +1,21 @@
 package com.github.springudemy.libraryapi.config;
 
+import com.github.springudemy.libraryapi.security.CustomRegisteredClientRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.time.Duration;
 
@@ -21,7 +31,37 @@ import java.time.Duration;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AuthorizationServerConfiguration {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception{
+
+        // Cria um configurador de servidor de autorização OAuth2
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = OAuth2AuthorizationServerConfigurer.authorizationServer();
+
+        // Aplica as configurações padrão para o Authorization Server
+        http.with(authorizationServerConfigurer, Customizer.withDefaults());
+
+        // Habilita o suporte a OpenID Connect (OIDC) no Authorization Server
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
+
+        // Configura o Resource Server para aceitar e validar token JWT
+        http.oauth2ResourceServer(oauth2Rs -> oauth2Rs.jwt(Customizer.withDefaults()));
+
+        // Configura uma página de login personalizada para autenticação
+        http.formLogin(configurer -> configurer.loginPage("/login"));
+
+        // Retorna a configuração de segurança aplicada
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
 
     @Bean
     public TokenSettings tokenSettings(){

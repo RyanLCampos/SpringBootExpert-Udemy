025280c58bd4f6698630341d9148714a22105abe
RyanLCampos <ryanlcampos19@gmail.com>
2025-03-24 16:11:59 -0300

    :rocket: Feat: cria projeto para modulo 'Dominando Docker' do curso

---
+++ convidados/src/main/java/com/github/RyanLCampos/convidados/ConvidadosApplication.java
@@ -0,0 +1,13 @@
+package com.github.RyanLCampos.convidados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvidadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvidadosApplication.class, args);
+	}
+
+}
